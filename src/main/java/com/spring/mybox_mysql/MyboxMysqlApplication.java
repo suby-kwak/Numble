c101b94b2681136debc484cd9fc04a3e8f8f1e40
suby-kwak <rhkdbtj@gmail.com>
2023-08-16 17:41:39 +0900

    [Init]: mybox_mysql 시작

---
+++ src/main/java/com/spring/mybox_mysql/MyboxMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mybox_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyboxMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyboxMysqlApplication.class, args);
+    }
+
+}
